b9f0f00f331181407e5fa6de5ec4afcd60883523
Stanley-Okwii <stanleeparker12@gmail.com>
2018-02-05 18:29:43 +0300

    initial setup

---
+++ connection.js
@@ -0,0 +1,34 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'weatherdata',
+  database : 'my_db'
+});
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+});
+
+connection.end();
+
+
+//recommended sql
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'example.org',
+  user     : 'bob',
+  password : 'secret'
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+
+  console.log('connected as id ' + connection.threadId);
+});
